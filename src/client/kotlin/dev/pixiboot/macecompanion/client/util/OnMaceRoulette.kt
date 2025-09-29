df47860f20a69952438a9ce24a116f75ed538e40
Pixi <realblahaj@proton.me>
2025-09-28 21:42:43 -0500

    refactor code a bit

--- src/client/kotlin/dev/pixiboot/macecompanion/client/util/OnMaceRoulette.kt
+++ src/client/kotlin/dev/pixiboot/macecompanion/client/util/OnMaceRoulette.kt
@@ -3,6 +3,7 @@ package dev.pixiboot.macecompanion.client.util
 import dev.dfonline.flint.FlintAPI
 import dev.dfonline.flint.feature.trait.PlotSwitchListeningFeature
 import dev.dfonline.flint.hypercube.Plot
+import dev.pixiboot.macecompanion.MaceCompanion
 import dev.pixiboot.macecompanion.client.StateManager
 
 object OnMaceRoulette {
@@ -10,11 +11,9 @@ object OnMaceRoulette {
     fun registerFlintFeature() {
         FlintAPI.confirmLocationWithLocate()
         FlintAPI.registerFeature(
-            object : PlotSwitchListeningFeature {
-                override fun onSwitchPlot(oldPlot: Plot, plot: Plot) {
-                    StateManager.resetState()
-                    onMace = setOf(14000002 /* Mace Roulette */, 14000004 /* MaceDev */, 14000007 /* MaceStatless (old) */, 70525 /* MaceStatless (new) */).contains(plot.id)
-                }
+            PlotSwitchListeningFeature { oldPlot, plot ->
+                StateManager.resetState()
+                onMace = setOf(14000002 /* Mace Roulette */, 14000004 /* MaceDev */, 14000007 /* MaceStatless (old) */, 70525 /* MaceStatless (new) */).contains(plot.id)
             }
         )
     }
