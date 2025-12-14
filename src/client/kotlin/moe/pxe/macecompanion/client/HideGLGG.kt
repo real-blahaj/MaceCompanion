5ecd11e03621258f9cc37fd1adef38e999c1383d
Pixi <realblahaj@proton.me>
2025-12-13 19:40:13 -0600

    GG/GL now hides regardless of color; also, it now uses OnMaceRoulette

--- src/client/kotlin/moe/pxe/macecompanion/client/HideGLGG.kt
+++ src/client/kotlin/moe/pxe/macecompanion/client/HideGLGG.kt
@@ -3,26 +3,26 @@ package moe.pxe.macecompanion.client
 import dev.isxander.yacl3.config.v3.value
 import moe.pxe.macecompanion.client.config.Config
 import moe.pxe.macecompanion.client.util.ContainsText
+import moe.pxe.macecompanion.client.util.OnMaceRoulette
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents
-import net.minecraft.text.Style
-import net.minecraft.text.Text
 
 object HideGLGG {
     var glTexts = listOf(
-        Text.literal("GOOD LUCK").setStyle(Style.EMPTY.withColor(0xFFCE2D).withBold(true)),
-        Text.literal("HAVE FUN").setStyle(Style.EMPTY.withColor(0xFFCE2D).withBold(true)),
-        Text.literal("GL").setStyle(Style.EMPTY.withColor(0xFFCE2D).withBold(true)),
-        Text.literal("HF").setStyle(Style.EMPTY.withColor(0xFFCE2D).withBold(true)),
+        "GOOD LUCK",
+        "HAVE FUN",
+        "GL",
+        "HF",
     )
     var ggTexts = listOf(
-        Text.literal("GOOD GAME").setStyle(Style.EMPTY.withColor(0xA0F9FF).withBold(true)),
-        Text.literal("GG").setStyle(Style.EMPTY.withColor(0xA0F9FF).withBold(true))
+        "GOOD GAME",
+        "GG"
     )
     fun registerListener() {
         ClientReceiveMessageEvents.ALLOW_GAME.register { message, overlay ->
+            if (!OnMaceRoulette.onMace) return@register true
             if (overlay) return@register true
-            if (Config.hideGLMessages.value) glTexts.forEach { if (ContainsText.containsText(message, it)) return@register false }
-            if (Config.hideGGMessages.value) ggTexts.forEach { if (ContainsText.containsText(message, it)) return@register false }
+            if (Config.hideGLMessages.value) glTexts.forEach { if (ContainsText.boldString(message, it)) return@register false }
+            if (Config.hideGGMessages.value) ggTexts.forEach { if (ContainsText.boldString(message, it)) return@register false }
             return@register true
         }
     }
--- src/client/kotlin/moe/pxe/macecompanion/client/util/ContainsText.kt
+++ src/client/kotlin/moe/pxe/macecompanion/client/util/ContainsText.kt
@@ -3,12 +3,22 @@ package moe.pxe.macecompanion.client.util
 import net.minecraft.text.Text
 
 object ContainsText {
-    fun containsText(text: Text, subtext: Text): Boolean {
+    fun strict(text: Text, subtext: Text): Boolean {
         if (text.string.contains(subtext.string) && text.style.equals(subtext.style)) {
             return true
         }
         for (child in text.siblings) {
-            if (containsText(child, subtext)) return true
+            if (strict(child, subtext)) return true
+        }
+        return false
+    }
+
+    fun boldString(text: Text, subtext: String): Boolean {
+        if (text.string.contains(subtext) && text.style.isBold) {
+            return true
+        }
+        for (child in text.siblings) {
+            if (boldString(child, subtext)) return true
         }
         return false
     }
